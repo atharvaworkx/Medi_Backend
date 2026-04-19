f4b781f8d48324b957c1c1288032e9f9ab4f9dc1
im-AtharvaWarankar <atharva.warankar@atomicloops.com>
2026-04-19 18:18:59 +0530

    Fix: Add debug info to start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "Starting Django server..."
 export DJANGO_SETTINGS_MODULE=src.settings.prod
+export ENV=prod
+python -c "import django; print('Django version:', django.get_version())"
+python -c "import src.settings.prod; print('Settings loaded successfully')"
+python manage.py check --deploy
 python manage.py runserver 0.0.0.0:8000 --noreload
